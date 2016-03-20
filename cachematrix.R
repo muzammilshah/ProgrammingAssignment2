9476acb4deb19a4d5ad9fd6a2e969aea043351eb
muzammilshah <muzammilshah@hotmail.com>
2016-03-20 22:57:30 +0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(m = matrix()){
+##m in square matrix
+##makeCacheMatrix function that will contain 4 sub functions namely:
+##get, set, getinv, setinv  
 
-## Write a short comment describing this function
+inv = NULL  ##set the value to Null and initialize the variable
+  
+  
+get <- function(){
+  return(m)
+}
+##function simply returns what is stored in m
 
-makeCacheMatrix <- function(x = matrix()) {
+set <- function(y){
+  m <<- y
+}
+##function will set in m the value given as an argument to the set funtion call
+
+
+
+
+setinv <- function(inverse){
+  inv <<- inverse
+  }
+##function the value of inverse as given in the argument of the function call
+
+getinv <-function(){
+  return(inv)
+  }
+
+##function will return the inverse of a matrix value as stored
+
+list(get = get, set = set, setinv = setinv, getinv = getinv) ##list will store the names of the subfunctions
 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+cachesolve <- function(m, ...){
+  inv = m$getinv()
+  
+  if(!is.null(inv)){
+    message("Getting Cached Data...")
+    
+    }
+  
+  else{
+    
+    inv = solve(m$get(), ...)
+  }
+  
+  m$setinv(inv)
+  return(inv)    
 }
